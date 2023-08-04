6c286deaaaee7441cc4d5f910bc01a8ee4702203
bdistin <bdistin@gmail.com>
2023-08-04 17:01:18 -0500

    null defaultViewport

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ app.get('/', async (request, response) => {
 		}
 		response
 			.set('Content-Type', 'image/png')
-			.send(await getImage(request.query.zip, request.query.width || 640, request.query.height || 480));
+			.send(await getImage(request.query.zip));
 	} catch (error) {
 		console.log(error);
 	}
 });
 
-async function getImage(zip, width, height) {
+async function getImage(zip) {
 	const browser = await puppeteer.launch({
 		args: ['--no-sandbox'],
-		headless: 'new'
+		headless: 'new',
+		defaultViewport: null
 	});
 	const page = await browser.newPage();
-	await page.setViewport({ width, height });
 	await page.goto(`https://weather.com/weather/tenday/l/${zip}`);
 	await page.waitForSelector('#WxuDailyCard-main-a43097e1-49d7-4df7-9d1a-334b29628263 > section');
 	const element = await page.$('#WxuDailyCard-main-a43097e1-49d7-4df7-9d1a-334b29628263 > section');
